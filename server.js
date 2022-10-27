da9794f49a555079f7fb8441a76f672703e7f5b2
Monica <ramirez.92.monica@gmail.com>
2022-10-26 23:38:43 -0500

    movie server running

--- server.js
+++ server.js
@@ -76,20 +76,30 @@ class Forecast{
     this.description = dayObj.weather.description;
   }
 }
-// app.get('/movies',async(request,response,next)=>{
+app.get('/movies',async(request,response,next)=>{
+  try{
+  let movieCity = request.query.movieCity
   
-//   let movieCity = request.query.movieCity
-//   let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&&query=#${movieCity}`
+  let movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${movieCity}`
   
-//   let movieResults = await axios.get(movieUrl);
-// });
+  let movieResults = await axios.get(movieUrl);
 
-// class Movies {
-//   constructor(movieObj){
-//     this.title = movieObject
+  let  groomedData = movieResults.data.results.map(movie =>new Movie(movie));
+  response.status(200).send(groomedData);
 
-//   }
-// }
+  }catch(error){
+    console.log(error);
+    next(error);
+  }
+});
+
+class Movie {
+  constructor(movieObj){
+    this.title = movieObj.title;
+    this.overview = movieObj.overview;
+    this.posterPath =movieObj.posterPath;
+  }
+}
 //  '*'=catch all, catch anything MUST LIVE IN BOTTOM 
 app.get('*', (request, response)=> {
   response.status(404).send('this route doesnt exist');
